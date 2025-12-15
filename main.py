b448b9ce50826c3cea7c0d60aac2d9651a09fe4c
Diandra Davidson <15893653+diandra-davidson@users.noreply.github.com>
2025-12-15 18:28:36 -0500

    Added landing page and separated about.html

--- main.py
+++ main.py
@@ -14,12 +14,30 @@ def main() -> None:
 @app.route('/about', methods=['GET']) # type: ignore
 def about() -> None:
     """About page."""
+    return render_template('about.html')  # type: ignore
+
+
+@app.route('/services', methods=['GET']) # type: ignore
+def services() -> None:
+    """Services page."""
+    return render_template('index.html')  # type: ignore
+
+
+@app.route('/experience', methods=['GET']) # type: ignore
+def experience() -> None:
+    """Work Experience page."""
     return render_template('index.html')  # type: ignore
 
 
 @app.route('/portfolio', methods=['GET']) # type: ignore
 def portfolio() -> None:
     """Portfolio page."""
+    return render_template('index.html')  # type: ignore
+
+
+@app.route('/contact', methods=['GET']) # type: ignore
+def contact() -> None:
+    """Contact page."""
     return render_template('index.html')  # type: ignore
 
 

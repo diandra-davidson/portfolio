dc1f651e550bfb8bf5b10c76f3487da11f142e1a
Diandra Davidson <15893653+diandra-davidson@users.noreply.github.com>
2026-04-30 20:04:53 +0000

    KAN-5: Fixing Bootstrap5 initialization

--- __init__.py
+++ __init__.py
@@ -1,20 +1,14 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap5 # type: ignore
+from flask_bootstrap import Bootstrap5
 
+bootstrap = Bootstrap5()
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
 
+    bootstrap.init_app(app)
+
     from .main import bp
     app.register_blueprint(bp)
-    
+
     return app
-
-
-def setup_bootstrap(app: Flask) -> None:
-    Bootstrap5(app)
-    return
-
-
-app = create_app()
-setup_bootstrap(app)
